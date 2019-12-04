6579b47451c5cc86823c6e0cba85036ee1ac7489
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-12-04 11:40:43 +0100

    Update buffer.hpp

--- buffer.hpp
+++ buffer.hpp
@@ -4,6 +4,9 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+Buffer(int size);
+Buffer();
+~Buffer();
 cv::Mat* get_buffer();
 void clear_buffer();
 bool is_full();
@@ -11,8 +14,7 @@ bool is_empty();
 cv::Mat get_last_mat();
 void set_last_mat(cv::Mat image);
 int get_buffer_size();
-Buffer(int size);
-Buffer();
+
 cv::Mat* get_part_buffer(int a,int b);
 
 
