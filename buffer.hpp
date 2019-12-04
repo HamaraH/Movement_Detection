fcfa5639789e74a012851046aaf3a53efaceb7a6
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-12-04 11:29:16 +0100

    Update buffer.hpp

--- buffer.hpp
+++ buffer.hpp
@@ -4,15 +4,15 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
-Mat* get_buffer(){};
-void clear_buffer(){};
-boolean is_full(){};
-Mat get_last_mat(){};
-void set_last_mat(Mat image){};
-int get_buffer_size(){};
-Buffer(int size){};
-Buffer(){};
+Mat* get_buffer();
+void clear_buffer();
+boolean is_full();
+Mat get_last_mat();
+void set_last_mat(Mat image);
+int get_buffer_size();
+Buffer(int size);
+Buffer();
 Mat* get_part_buffer(int a,b);
-boolean is_empty(){};
+boolean is_empty();
 
 #endif
