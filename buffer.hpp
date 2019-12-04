d052220f91ce240c161eb19f2857f0d66b8fafc4
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-12-04 11:33:08 +0100

    Update buffer.hpp

--- buffer.hpp
+++ buffer.hpp
@@ -1,18 +1,18 @@
 
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
 
 #ifndef BUFFER_H
 #define BUFFER_H
 
-Mat* get_buffer();
+cv::Mat* get_buffer();
 void clear_buffer();
 boolean is_full();
-Mat get_last_mat();
-void set_last_mat(Mat image);
+cv::Mat get_last_mat();
+void set_last_mat(cv::Mat image);
 int get_buffer_size();
 Buffer(int size);
 Buffer();
-Mat* get_part_buffer(int a,b);
+cv::Mat* get_part_buffer(int a,b);
 boolean is_empty();
 
 #endif
